720cc77ab563cf72732a04203a17c110ca2882fa
jonaniano <spamjonhere@yahoo.com>
2018-03-08 11:10:55 -0800

    Cleaned stuff in app.js and added logging

--- app.js
+++ app.js
@@ -3,22 +3,6 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 app.use(express.static('views/assets'));
-/*
-var Zendesk = require('zendesk-node-api');
-
-var zendesk = new Zendesk({
-  url: 'https://billmcpherson.zendesk.com',
-  email: 'billmcpherson87@gmail.com', 
-  token: 'PnqQEsITIETSXoN3xGcNruabkS5keKx5rqX2YhAn'
-  });
-
-zendesk.objects.list(
-  // (Optional) URL params available for each object.
-  // https://developer.zendesk.com/rest_api/docs/core/
-).then(function(result){
-    console.log(result);
-});
-*/
 
 var zendesk = require('node-zendesk');
 
@@ -36,26 +20,9 @@ router.get("/2",function(req,res){
   res.sendFile(path + "index2.html");
 });
 
-router.get("/assets",function(req,res){
-  res.sendFile(path + "/assets");
-});
-
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
-
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
-});
-
 router.get("/zeninfo",function(req,res){
 	var custEmail = req.query.email;
 	var query = "type:user email:" + custEmail;
-	console.log("in zeninfo");
-    console.log("debugreq:");
-	console.log(req);
-    console.log("debugres:");
-	console.log(res);
 	client.search.query(query, function (err, req, result) {
 		console.log("debuginquery");
 		var userId;
@@ -90,6 +57,7 @@ app.use("/",router);
 
 app.use("*",function(req,res){
   res.sendFile(path + "404.html");
+  console.log('Failed Request Made for: ' + req.baseUrl);
 });
 
 app.listen(8080,function(){
